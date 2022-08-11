32322ccca185129da54ba90f2b91800a834a863e
Xhulio Bekteshi <101073879+XhulioBekteshi@users.noreply.github.com>
2022-08-11 17:46:41 +0200

    commit
    commit

---
+++ project 1 /assets/navbar-effect.js
@@ -0,0 +1,11 @@
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_navbar_hide_scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-77px";
+  }
+  prevScrollpos = currentScrollPos;
+}
